e2f695c942ff56653a3871dca3b442860366ee9f
HuangSam <1032479745@qq.com>
2019-08-12 10:25:46 +0800

    no message

--- golang/lib/utils/socketlib/core/polling_transport.go
+++ golang/lib/utils/socketlib/core/polling_transport.go
@@ -1,1 +1,18 @@
 package core
+
+import (
+	"net/http"
+	"time"
+)
+
+//PollingTransport 轮询传输
+type PollingTransport struct {
+	Client *http.Client
+}
+
+// Default 默认
+var Default = &PollingTransport{
+	Client: &http.Client{
+		Timeout: time.Minute,
+	},
+}
