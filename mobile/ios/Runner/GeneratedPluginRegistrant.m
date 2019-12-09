d92cf448d381a19fa0704985a350fd04cd905238
znk1007 <1032479745@qq.com>
2019-12-09 09:44:56 +0800

    0.0.1

--- mobile/ios/Runner/GeneratedPluginRegistrant.m
+++ mobile/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,9 +3,24 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<device_info/DeviceInfoPlugin.h>)
 #import <device_info/DeviceInfoPlugin.h>
+#else
+@import device_info;
+#endif
+
+#if __has_include(<package_info/PackageInfoPlugin.h>)
 #import <package_info/PackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
 #import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
