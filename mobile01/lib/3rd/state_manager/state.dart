c95a0a8907f606045cb1daf7b1507eaa850e7a7d
znk1007 <1032479745@qq.com>
2019-12-13 18:41:49 +0800

    0.0.1

---
+++ mobile01/lib/3rd/state_manager/state.dart
@@ -0,0 +1,9 @@
+export 'src/async_state.dart';
+export 'src/change_notifier_state.dart';
+export 'src/consumer.dart';
+export 'src/delegate_widget.dart';
+export 'src/listenable_state.dart';
+export 'src/value_listenable_state.dart';
+export 'src/proxy_state.dart' hide NumericProxyProvider, Void, ProxyProviderBase;
+export 'src/selector.dart';
+export 'src/base_state.dart';
