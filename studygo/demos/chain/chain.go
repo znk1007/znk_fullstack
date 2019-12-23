a728957937359457ff0271cfa480ca908cd2cd12
znk1007 <1032479745@qq.com>
2019-12-23 22:28:34 +0800

    0.0.1

--- studygo/demos/chain/chain.go
+++ studygo/demos/chain/chain.go
@@ -4,7 +4,6 @@ type LinkedList struct {
 	size int
 	single bool
 	head *LinkedListNode
-	tail *LinkedListNode
 }
 
 /*链表节点对象*/
@@ -20,46 +19,92 @@ func CreateLinkedList(single bool) *LinkedList  {
 		size:   0,
 		single: single,
 		head:   nil,
-		tail:   nil,
 	}
 	return linkedList
 }
 
+func (list *LinkedList)insert(byHead bool, datas ...interface{}) bool {
+	dataLen := len(datas)
+	if dataLen == 0 {
+		 return false
+	}
+	node := list.head
+	for i := 0; i < dataLen; i++ {
+		newNode := &LinkedListNode{
+			data: datas[i],
+			prev: nil,
+			next: nil,
+		}
+		if node == nil {
+			list.head = newNode
+			node = list.head
+		}
+		
+	}
+}
+
 /*插入节点*/
-func (list *LinkedList)InsertLinkedListNode(data interface{}, index int, byHead bool) bool  {
-	if list == nil || data == nil || list.size < index {
+func (list *LinkedList)InsertLinkedListNode(index int, byHead bool, datas ...interface{}) bool  {
+	if list == nil || datas == nil || len(datas) == 0 || list.size < index {
 		return false
 	}
 	if list.single {
 		if list.size == 0 {
-			list.head = &LinkedListNode{
-				data: data,
-			}
-			list.size++
-		} else {
-			tempNode := list.head
-			if index < 1 || index > list.size {
-				for tempNode != nil {
-					tempNode = tempNode.next
+			node := list.head
+			for i := 0; i < len(datas); i++ {
+				newNode := &LinkedListNode{
+					data: datas[i],
 				}
-
+				if list.head == nil {
+					list.head = newNode
+					node = newNode
+				}
+				if byHead {
+					newNode.next = node.next
+					node.next = newNode
+				} else {
+					node.next = newNode
+					node = newNode
+				}
+				list.size++
+			}
+		} else {
+			node := list.head
+			if index < 1 || index >= list.size {
+				newNode := &LinkedListNode{
+					data: data,
+					prev: nil,
+					next: nil,
+				}
+				if byHead {
+					newNode.next = node.next
+					node.next = newNode
+				} else {
+					for node.next != nil {
+						node = node.next
+					}
+					node.next = newNode
+				}
+				list.size++
+				return true
 			}
 			i := 1
-			for i < index || tempNode != nil  {
-				tempNode = tempNode.next
+			for i < index || node != nil  {
+				node = node.next
 				i++
 			}
-			if i > index || tempNode == nil {
+			if i > index || node == nil {
 				return false
 			}
-			node := &LinkedListNode{
+			newNode := &LinkedListNode{
 				data: data,
 				prev: nil,
 				next: nil,
 			}
-			node.next = tempNode.next
-			tempNode.next = node
+			newNode.next = node.next
+			node.next = newNode
 			list.size++
+			return true
 		}
 	} else {
 		
