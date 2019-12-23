e6a48a9c2ebbe27095346861dad66b844ce20c5f
znk1007 <1032479745@qq.com>
2019-12-23 14:07:35 +0800

    0.0.1

---
+++ studygo/demos/chain/chain.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+}
+/*单链表节点对象*/
+type LinkedListNode struct {
+	 data interface{}
+	 prev *LinkedListNode
+	 next *LinkedListNode
+}
+/*头节点*/
+var head *LinkedListNode
+
+/*创建链表节点*/
+func createLinkedListNode(data ...interface{})  {
+
+}
+
+
+
