835dce596740ab8849bad12d87c356a6f55e5135
znk1007 <1032479745@qq.com>
2019-12-24 15:13:52 +0800

    0.0.1

--- studygo/demos/chain/chain.go
+++ studygo/demos/chain/chain.go
@@ -1,8 +1,12 @@
 package chain
+
+import "fmt"
+
 /*链表对象*/
 type LinkedList struct {
 	size int
 	single bool
+	close bool
 	head *LinkedListNode
 }
 
@@ -14,104 +18,143 @@ type LinkedListNode struct {
 }
 
 /*创建链表*/
-func CreateLinkedList(single bool) *LinkedList  {
+func CreateLinkedList(single bool, close bool) *LinkedList  {
 	linkedList := &LinkedList{
 		size:   0,
 		single: single,
+		close:close,
 		head:   nil,
 	}
 	return linkedList
 }
 
-func (list *LinkedList)insert(byHead bool, datas ...interface{}) bool {
-	dataLen := len(datas)
-	if dataLen == 0 {
-		 return false
+/*插入节点*/
+func (list *LinkedList)InsertLinkedListNode(index int, data interface{}) bool  {
+	if list == nil || data == nil {
+		return false
 	}
-	node := list.head
-	for i := 0; i < dataLen; i++ {
+	if index < 0 {
+		list.insertHead(data)
+	} else if index > list.size {
+		list.insertTail(data)
+	} else {
+		node := list.head
+		for i := 0; i < index - 1; i++ {
+			if node == nil {
+				return false
+			}
+			node = node.next
+		}
 		newNode := &LinkedListNode{
-			data: datas[i],
+			data: data,
 			prev: nil,
 			next: nil,
 		}
-		if node == nil {
-			list.head = newNode
-			node = list.head
+		newNode.next = node.next
+		node.next = newNode
+		if list.single == false {
+			newNode.prev = node
 		}
-		
+		list.size++
+	}
+	return false
+}
+/*链表长度*/
+func (list *LinkedList) Length() int {
+	return list.size
+}
+
+/*打印链表数据*/
+func (node *LinkedListNode)Print(loop bool)  {
+	if node == nil {
+		return
+	}
+	fmt.Println("----start----")
+	if node.prev != nil {
+		fmt.Println("prev node data: ", node.prev.data)
+	} else {
+		fmt.Println("prev node is nil")
+	}
+	fmt.Println("current node data: ", node.data)
+	if node.next != nil {
+		fmt.Println("next node data: ", node.next.data)
+	} else {
+		fmt.Println("next node is nil")
+	}
+
+	fmt.Println("----end----")
+	if loop {
+		node.next.Print(loop)
 	}
 }
 
-/*插入节点*/
-func (list *LinkedList)InsertLinkedListNode(index int, byHead bool, datas ...interface{}) bool  {
-	if list == nil || datas == nil || len(datas) == 0 || list.size < index {
-		return false
+/*头部节点*/
+func (list *LinkedList) Head() *LinkedListNode  {
+	return list.head
+}
+
+func (list *LinkedList)Get(index int) *LinkedListNode {
+	if list == nil || list.size == 0 || index >= list.size {
+		return nil
+	}
+	if index == 0 {
+		 return list.head
+	}
+	node := list.head
+	for i := 1; i <= index; i++ {
+		if node == nil {
+			 return nil
+		}
+		node = node.next
+	}
+	return node
+}
+
+/*插入头部*/
+func (list *LinkedList)insertHead(data interface{})  {
+	if list == nil || data == nil {
+		return
+	}
+	node := &LinkedListNode{
+		data: data,
+		prev: nil,
+		next: nil,
 	}
 	if list.single {
-		if list.size == 0 {
-			node := list.head
-			for i := 0; i < len(datas); i++ {
-				newNode := &LinkedListNode{
-					data: datas[i],
-				}
-				if list.head == nil {
-					list.head = newNode
-					node = newNode
-				}
-				if byHead {
-					newNode.next = node.next
-					node.next = newNode
-				} else {
-					node.next = newNode
-					node = newNode
-				}
-				list.size++
-			}
-		} else {
-			node := list.head
-			if index < 1 || index >= list.size {
-				newNode := &LinkedListNode{
-					data: data,
-					prev: nil,
-					next: nil,
-				}
-				if byHead {
-					newNode.next = node.next
-					node.next = newNode
-				} else {
-					for node.next != nil {
-						node = node.next
-					}
-					node.next = newNode
-				}
-				list.size++
-				return true
-			}
-			i := 1
-			for i < index || node != nil  {
-				node = node.next
-				i++
-			}
-			if i > index || node == nil {
-				return false
-			}
-			newNode := &LinkedListNode{
-				data: data,
-				prev: nil,
-				next: nil,
-			}
-			newNode.next = node.next
-			node.next = newNode
-			list.size++
-			return true
-		}
+		node.next = list.head
+		list.head = node
+		list.size++
 	} else {
-		
+		node.next = list.head
+		if list.head != nil {
+			node.prev = list.head.prev
+		}
+		list.head = node
+		list.size++
 	}
-	
-	return false
+
 }
+
+/*插入尾部*/
+func (list *LinkedList)insertTail(data interface{})  {
+	node := &LinkedListNode{
+		data: data,
+		prev: nil,
+		next: nil,
+	}
+	if list.head == nil {
+		list.head = node
+	} else {
+		current := list.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = node
+		node.prev = current
+	}
+	list.size++
+}
+
 
 
 
--- studygo/demos/main.go
+++ studygo/demos/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"github.com/znk_fullstack/studygo/demos/chain"
+)
+
 func main() {
+	list := chain.CreateLinkedList(true,false)
+	for i := 0; i < 12; i++ {
+		list.InsertLinkedListNode(-1, i)
+	}
+	head := list.Head()
+	head.Print(true)
+	fmt.Println("linked list length", list.Length())
+
+	fmt.Println("+++++++++++++++++++++")
+
+	list.InsertLinkedListNode(5, 20)
+
+	head.Print(true)
+	fmt.Println("linked list length", list.Length())
+
+	node := list.Get(5)
+	node.Print(false)
 
 }
