3da22f9b33dd96a8dcfcfca33336a6395429c2e6
znk1007 <1032479745@qq.com>
2020-01-12 22:15:09 +0800

    0.0.1

---
+++ studygo/demos/eighth/homework08.go
@@ -0,0 +1,23 @@
+package eighth
+
+/*
+ * @Author: your name
+ * @Date: 2020-01-12 21:33:47
+ * @LastEditTime : 2020-01-12 22:14:48
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /demos/eighth/homework08.go
+ */
+
+//Coder 攻城狮
+type Coder struct {
+	Name      string //名称
+	Education string //学历
+	School    string // 学校
+	Major     string // 专业
+	Years     string // 工作年限
+	Job       string // 职位
+	Salary    string // 薪水
+	Language  string // 编程语言
+}
+
