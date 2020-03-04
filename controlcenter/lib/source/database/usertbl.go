49a6c96d9e78f8076a5bcef51808a321834fad0a
znk1007 <1032479745@qq.com>
2020-03-04 22:26:44 +0800

    0.0.1

--- controlcenter/lib/source/database/usertbl.go
+++ controlcenter/lib/source/database/usertbl.go
@@ -13,6 +13,7 @@ type CCUserTBL struct {
 	Username string `gorm:"type:varchar(100)"`
 	Account  string `gorm:"type:varchar(50)"`
 	Password string `gorm:"type:varchar(100)"`
+	Phone    string `gorm:"type:varchar(20)"`
 }
 
 //CreateUserTBL 创建用户表
@@ -25,19 +26,39 @@ func CreateUserTBL() error {
 	return nil
 }
 
-//InsertUser 插入用户数据
-func InsertUser(user *CCUserTBL) error {
+//UpsertUser 插入用户数据
+func UpsertUser(user *CCUserTBL) error {
 	if dbConn == nil {
 		return errors.New("Connect database first")
+	}
+	var u *CCUserTBL
+	dbConn.db.Where("userID = ?", user.UserID).First(&u)
+	if u != nil && (u.UserID == user.UserID) {
+		dbConn.db.Model(&u).Updates(CCUserTBL{Phone: user.Phone, Username: user.Username})
 	}
 	dbConn.db.Create(&user)
 	return nil
 }
 
 //UpdateUser 更新用户信息
-func UpdateUser() error {
+func UpdateUser(userID string, values map[string]interface{}) error {
 	if dbConn == nil {
 		return errors.New("Connect database first")
 	}
-	dbConn.db.Update()
+	var user *CCUserTBL
+	dbConn.db.Model(&user).Where("userId = ?", userID).Updates(values)
+	return nil
+}
+
+//FindUser 查询用户第一条数据
+func FindUser(userID string) (*CCUserTBL, error) {
+	if dbConn == nil {
+		return nil, errors.New("Connect database first")
+	}
+	var u *CCUserTBL
+	dbConn.db.Where("userID = ?", userID).First(&u)
+	if u == nil {
+		return nil, errors.New("user not exists")
+	}
+	return u, nil
 }
