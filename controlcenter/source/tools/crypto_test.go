080809e7bd974e66ea85899a1cbb25bbeaeb1b2e
znk1007 <1032479745@qq.com>
2020-03-11 16:31:12 +0800

    0.0.2

---
+++ controlcenter/source/tools/crypto_test.go
@@ -0,0 +1,18 @@
+package tools
+
+import "testing"
+
+func TestCBCCrypto(t *testing.T) {
+	org := "测试溜溜溜儿"
+	res, err := CBCEncrypt(org)
+	if err != nil {
+		t.Error("cbc encrypt err: ", err)
+		return
+	}
+	org, err = CBCDecrypt(res)
+	if err != nil {
+		t.Error("cbc decrypt err: ", err)
+		return
+	}
+	t.Log("org: ", org)
+}
