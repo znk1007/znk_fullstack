77092b00aa5fc06bfb794769f6c336fd484bdbac
znk1007 <1032479745@qq.com>
2020-03-11 09:13:35 +0800

    0.0.1

---
+++ flutter_plugins/znk_auth/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<znk_auth/ZnkAuthPlugin.h>)
+#import <znk_auth/ZnkAuthPlugin.h>
+#else
+@import znk_auth;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ZnkAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"ZnkAuthPlugin"]];
+}
+
+@end
