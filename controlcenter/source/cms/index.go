79d0ffb4621390da01b1fe8c49cec858d5ec0d4a
znk1007 <1032479745@qq.com>
2020-03-12 19:06:39 +0800

    0.0.2

--- controlcenter/source/cms/index.go
+++ controlcenter/source/cms/index.go
@@ -22,12 +22,12 @@ func firstVersion() {
 	fp := tools.GetFilePathFromCurrent("view/cms")
 	fmt.Println("file path: ", fp)
 	tools.Gt.Router.LoadHTMLGlob(fp + "/html/*")
-	tools.Gt.Router.Static("/static", fp+"/")
+	tools.Gt.Router.Static("", fp+"/")
 
 	vGroup := tools.Gt.Router.Group("/v1")
 
 	cmsGroup := vGroup.Group("/cms")
-	cmsGroup.GET("", controller.Home)
+	cmsGroup.GET("/static", controller.Home)
 	cmsGroup.POST("/regist", controller.Regist)
 	cmsGroup.POST("/login", controller.Login)
 }
