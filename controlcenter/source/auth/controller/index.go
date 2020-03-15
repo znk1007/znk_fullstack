d451582bf5164a2ae89b4ed58ff75fe40af144d3
znk1007 <1032479745@qq.com>
2020-03-15 22:18:47 +0800

    0.0.2

---
+++ controlcenter/source/auth/controller/index.go
@@ -0,0 +1,11 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Home(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"msg": "home page"})
+}
--- controlcenter/source/auth/index.go
+++ controlcenter/source/auth/index.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"github.com/znk_fullstack/controlcenter/source/auth/controller"
 	"github.com/znk_fullstack/controlcenter/source/tools"
 )
 
@@ -10,5 +11,6 @@ func Start() {
 	tools.LoadHTMLS(fp + "/html/*")
 	tools.LoadStatic("assets", fp+"/assets")
 	vGroup := tools.Gt.V1
-	vGroup.POST("/login")
+	vGroup.GET("/znksys",controller.Home)
+	// vGroup.POST("/login")
 }
