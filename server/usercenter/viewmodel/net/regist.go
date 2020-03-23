e92b86fb2f3d83fd35619c9289fd4ce50012ffa6
znk1007 <1032479745@qq.com>
2020-03-23 22:39:08 +0800

    0.0.3

--- server/usercenter/viewmodel/net/regist.go
+++ server/usercenter/viewmodel/net/regist.go
@@ -3,15 +3,39 @@ package usernet
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	userproto "github.com/znk_fullstack/server/usercenter/model/protos/generated"
 )
 
+var (
+	reqChan chan *userproto.RegistReq
+	resChan chan *userproto.RegistRes
+)
+
+var rs *registService
+
+func init() {
+	reqChan = make(chan *userproto.RegistReq)
+	resChan = make(chan *userproto.RegistRes)
+	rs = &registService{}
+	go rs.run()
+}
+
 //RegistService 注册
-type RegistService struct {
+type registService struct {
+}
+
+func (rs *registService) run() {
+	for {
+		select {
+		case req := <-reqChan:
+			fmt.Println(req)
+		}
+	}
 }
 
 //UserReigst 注册
-func (rs *RegistService) UserReigst(context.Context, *userproto.RegistReq) (*userproto.RegistRes, error) {
+func (rs *registService) UserReigst(context.Context, *userproto.RegistReq) (*userproto.RegistRes, error) {
 	return nil, errors.New("regist failed")
 }
