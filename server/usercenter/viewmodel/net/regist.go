fe726bbdc7634232668247f89f50e08284bed926
znk1007 <1032479745@qq.com>
2020-03-23 22:26:26 +0800

    0.0.3

--- server/usercenter/viewmodel/net/regist.go
+++ server/usercenter/viewmodel/net/regist.go
@@ -1,5 +1,17 @@
 package usernet
 
+import (
+	"context"
+	"errors"
+
+	userproto "github.com/znk_fullstack/server/usercenter/model/protos/generated"
+)
+
 //RegistService 注册
 type RegistService struct {
+}
+
+//UserReigst 注册
+func (rs *RegistService) UserReigst(context.Context, *userproto.RegistReq) (*userproto.RegistRes, error) {
+	return nil, errors.New("regist failed")
 }
