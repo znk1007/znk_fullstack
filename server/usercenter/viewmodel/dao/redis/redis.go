bddf98629823003bf6371aa97485c49be8250a3d
znk1007 <1032479745@qq.com>
2020-03-26 22:27:29 +0800

    0.0.3

--- server/usercenter/viewmodel/dao/redis/redis.go
+++ server/usercenter/viewmodel/dao/redis/redis.go
@@ -14,6 +14,7 @@ import (
 	userconf "github.com/znk_fullstack/server/usercenter/viewmodel/conf"
 )
 
+//https://blog.csdn.net/itcats_cn/article/details/82391719
 type redisConf struct {
 	Env  userconf.Env `json:"env"`
 	Host string       `json:"host"`
@@ -51,9 +52,8 @@ func readConf() {
 
 //Set 保存数据
 func Set(key string, val interface{}, exp time.Duration) error {
-	if rds == nil {
-		log.Info().Msg("redis client not init")
-		return errors.New("redis client not init")
+	if err := checkRds(); err != nil {
+		return err
 	}
 	rds.Set(key, val, exp)
 	return nil
@@ -61,15 +61,30 @@ func Set(key string, val interface{}, exp time.Duration) error {
 
 //Get 取值
 func Get(key string) (val string, err error) {
-	if rds == nil {
-		log.Info().Msg("redis client not init")
-		val = ""
-		err = errors.New("redis client not init")
-		return
+	if err := checkRds(); err != nil {
+		return "", err
 	}
 
 	val, err = rds.Get(key).Result()
 	return
+}
+
+//HSet hash存值
+func HSet(key string, values ...interface{}) error {
+	if err := checkRds(); err != nil {
+		return err
+	}
+	rds.HSet(key, values...)
+	return redis.Nil
+}
+
+//checkRds 校验rds实例
+func checkRds() error {
+	if rds == nil {
+		log.Info().Msg("redis client not init")
+		return errors.New("redis client not init")
+	}
+	return nil
 }
 
 //readFile 获取指定文件地址
