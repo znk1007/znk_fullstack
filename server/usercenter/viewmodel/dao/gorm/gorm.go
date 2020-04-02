4828d6fa48af5c6374fedae8bc5f35e572a370a6
znk1007 <1032479745@qq.com>
2020-04-02 09:27:47 +0800

    0.0.3

--- server/usercenter/viewmodel/dao/gorm/gorm.go
+++ server/usercenter/viewmodel/dao/gorm/gorm.go
@@ -2,6 +2,7 @@ package usergorm
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"path"
 	"runtime"
@@ -31,6 +32,11 @@ var gdb *gorm.DB
 //ConnectMariaDB 连接MariaDB
 func ConnectMariaDB(envir userconf.Env) {
 	initMariaDB(envir)
+}
+
+//Table 指定表
+func Table(name string) *gorm.DB {
+	return gdb.Table(name)
 }
 
 //Close 关闭数据库
@@ -78,8 +84,32 @@ func initMariaDB(envir userconf.Env) {
 	}
 }
 
+//checkDB 校验db句柄
+func checkDB() error {
+	if gdb == nil {
+		log.Info().Msg("db object can't be nil")
+		return errors.New("db object can't be nil")
+	}
+	return nil
+}
+
 //readFile 获取指定文件地址
 func readFile(relativePath string) string {
 	_, curPath, _, _ := runtime.Caller(1)
 	return path.Join(path.Dir(curPath) + "/" + relativePath)
 }
+
+/*
+type User struct {
+  gorm.Model
+  Name         string
+  Age          sql.NullInt64
+  Birthday     *time.Time
+  Email        string  `gorm:"type:varchar(100);unique_index"`
+  Role         string  `gorm:"size:255"` // 设置字段大小为255
+  MemberNumber *string `gorm:"unique;not null"` // 设置会员号（member number）唯一并且不为空
+  Num          int     `gorm:"AUTO_INCREMENT"` // 设置 num 为自增类型
+  Address      string  `gorm:"index:addr"` // 给address字段创建名为addr的索引
+  IgnoreMe     int     `gorm:"-"` // 忽略本字段
+}
+*/
--- server/usercenter/viewmodel/dao/gorm/gorm_test.go
+++ server/usercenter/viewmodel/dao/gorm/gorm_test.go
@@ -1,1 +1,11 @@
 package usergorm
+
+import (
+	"testing"
+
+	userconf "github.com/znk_fullstack/server/usercenter/viewmodel/conf"
+)
+
+func TestConnectDB(t *testing.T) {
+	ConnectMariaDB(userconf.Dev)
+}
