2588802656250f09b71d039cd2ed79d4f198f18f
znk1007 <1032479745@qq.com>
2020-04-07 09:26:39 +0800

    0.0.3

--- server/usercenter/model/device.go
+++ server/usercenter/model/device.go
@@ -1,13 +1,38 @@
 package model
 
-import gorm "github.com/jinzhu/gorm"
+import (
+	usergorm "github.com/znk_fullstack/server/usercenter/viewmodel/dao/gorm"
+)
 
 //Device 设备信息
 type Device struct {
-	gorm.Model
+	DeviceID string `gorm:"primary_key"`
 	UserID   string
 	Platform string
-	DeviceID string
-	Trust    bool
-	Online   bool
+	Trust    int
+	Online   int
+}
+
+//CreateDevice 创建设备信息数据
+func CreateDevice(device *Device) (exs bool, msg string) {
+	exs = usergorm.DB().NewRecord(device)
+	if !exs {
+		usergorm.DB().Create(device)
+		exs = usergorm.DB().NewRecord(device)
+		msg = "create deivce info success"
+	} else {
+		exs = true
+		msg = "device info exists"
+	}
+	return
+}
+
+//UpdateTrust 更新设备信任状态
+func UpdateTrust(deviceID string, trust int) error {
+	return usergorm.DB().Model(&Device{DeviceID: deviceID}).Update("trust", trust).Error
+}
+
+//UpdateOnline 更新设备在线状态
+func UpdateOnline(deviceID string, online int) error {
+	return usergorm.DB().Model(&Device{DeviceID: deviceID}).Update("online", online).Error
 }
