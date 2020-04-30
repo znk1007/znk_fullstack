ca08202f2ca79dff51c068598b89c7ee0bb72ff8
znk1007 <1032479745@qq.com>
2020-04-30 22:35:11 +0800

    0.0.4

--- server/imcenter/go.mod
+++ server/imcenter/go.mod
@@ -3,6 +3,7 @@ module github.com/znk_fullstack/server/imcenter
 go 1.13
 
 require (
+	github.com/coreos/etcd v3.3.20+incompatible // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.4.0
 	google.golang.org/grpc v1.29.1
--- server/imcenter/source/controller/net/client/usercenter.go
+++ server/imcenter/source/controller/net/client/usercenter.go
@@ -1,13 +1,37 @@
 package imnetclient
 
 import (
+	imconf "github.com/znk_fullstack/server/imcenter/source/controller/conf"
 	userproto "github.com/znk_fullstack/server/imcenter/source/model/protos/generated"
 	"google.golang.org/grpc"
 )
 
-type ucSrv struct {
+const (
+	appkey = "fullstack-manznk"
+)
+
+//RegistUserCenter 注册用户中心
+func RegistUserCenter(acc, pwd string) (err error) {
+	ucEnv := imconf.GetUcEnv()
+	addr := ucEnv.Host + ":" + ucEnv.Port
+	creds, e := imconf.TLSCredentials()
+	if e != nil {
+		err = e
+		return
+	}
+	conn, e := grpc.Dial(
+		addr,
+		grpc.WithTransportCredentials(creds),
+		grpc.WithBlock(),
+	)
+	if e != nil {
+		err = e
+		return
+	}
+	return
 }
 
-func NewUCSrv(cc *grpc.ClientConn) {
+func NewUcSrv(cc *grpc.ClientConn) {
+
 	userproto.NewUserSrvClient(cc)
 }
