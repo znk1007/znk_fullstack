7e5ba289c3315327fc93d0bddc65f127ca952426
znk1007 <1032479745@qq.com>
2020-05-07 15:44:36 +0800

    0.0.4

--- server/socket/websocket/util/util.go
+++ server/socket/websocket/util/util.go
@@ -5,7 +5,9 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"io"
+	"net/http"
 	"strings"
+	"unicode/utf8"
 )
 
 var keyGUID = []byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
@@ -110,8 +112,8 @@ var isTokenOctet = [256]bool{
 	'~':  true,
 }
 
-//SkipSpace 跳过指定字节
-func SkipSpace(s string) (rest string) {
+//skipSpace 跳过指定字节
+func skipSpace(s string) (rest string) {
 	i := 0
 	for ; i < len(s); i++ {
 		if b := s[i]; b != ' ' && b != '\t' {
@@ -122,8 +124,8 @@ func SkipSpace(s string) (rest string) {
 	return
 }
 
-//NextToken 返回符合 rfc 2616 规则的令牌喝遵循令牌协议的字符串
-func NextToken(s string) (token, rest string) {
+//nextToken 返回符合 rfc 2616 规则的令牌喝遵循令牌协议的字符串
+func nextToken(s string) (token, rest string) {
 	i := 0
 	for ; i < len(s); i++ {
 		if !isTokenOctet[s[i]] {
@@ -135,11 +137,10 @@ func NextToken(s string) (token, rest string) {
 	return
 }
 
-//NextTokenOrQuoted RFC 2616主要标志或引用字符串
-func NextTokenOrQuoted(s string) (val, rest string) {
+//nextTokenOrQuoted RFC 2616主要标志或引用字符串
+func nextTokenOrQuoted(s string) (val, rest string) {
 	if !strings.HasPrefix(s, "\"") {
-		val, rest = NextToken(s)
-		return
+		return nextToken(s)
 	}
 	s = s[1:]
 	for i := 0; i < len(s); i++ {
@@ -147,8 +148,76 @@ func NextTokenOrQuoted(s string) (val, rest string) {
 		case '"':
 			return s[:i], s[i+1:]
 		case '\\':
-
+			p := make([]byte, len(s)-1)
+			j := copy(p, s[:i])
+			escape := true
+			for i = i + 1; i < len(s); i++ {
+				b := s[i]
+				switch {
+				case escape:
+					escape = false
+					p[j] = b
+					j++
+				case b == '\\':
+					escape = true
+				case b == '"':
+					return string(p[:j]), s[i+1:]
+				default:
+					p[j] = b
+					j++
+				}
+			}
+			return "", ""
 		}
 	}
-	return
+	return "", ""
+}
+
+//equalASCIIFold s和t对比是否满足 RFC 4790协议中的ASCII定义要求
+func equalASCIIFold(s, t string) bool {
+	for s != "" && t != "" {
+		sr, size := utf8.DecodeRuneInString(s)
+		s = s[size:]
+		tr, size := utf8.DecodeRuneInString(t)
+		t = t[size:]
+		if sr == tr {
+			continue
+		}
+		if 'A' <= sr && sr <= 'Z' {
+			sr = sr + 'a' - 'A'
+		}
+		if 'A' <= tr && tr <= 'Z' {
+			tr = tr + 'a' - 'A'
+		}
+		if sr != tr {
+			return false
+		}
+	}
+	return s == t
+}
+
+//TokenListContainsValue 令牌是否包含给定的字符串
+func TokenListContainsValue(header http.Header, name, value string) bool {
+headers:
+	for _, s := range header[name] {
+		for {
+			var t string
+			t, s = nextToken(skipSpace(s))
+			if t == "" {
+				continue headers
+			}
+			s = skipSpace(s)
+			if s != "" && s[0] != ',' {
+				continue headers
+			}
+			if equalASCIIFold(t, value) {
+				return true
+			}
+			if s == "" {
+				continue headers
+			}
+			s = s[1:]
+		}
+	}
+	return false
 }
