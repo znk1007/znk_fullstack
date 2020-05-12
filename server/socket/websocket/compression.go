a05c58bc0acfeb81c94d80548dc3c15de6eb9214
znk1007 <1032479745@qq.com>
2020-05-12 18:52:23 +0800

    0.0.4

--- server/socket/websocket/compression.go
+++ server/socket/websocket/compression.go
@@ -3,6 +3,7 @@ package ws
 import (
 	"compress/flate"
 	"io"
+	"strings"
 	"sync"
 )
 
@@ -13,14 +14,58 @@ const (
 )
 
 var (
-	flateWriterPools [maxCompressionLevel-minCompressionLevel+1]sync.Pool
-	flateReaderPool = sync.Pool{
+	flateWriterPools [maxCompressionLevel - minCompressionLevel + 1]sync.Pool
+	flateReaderPool  = sync.Pool{
 		New: func() interface{} {
 			return flate.NewReader(nil)
 		},
 	}
 )
 
+func decompressNoContextTakeover(r io.Reader) io.ReadCloser {
+	const tail =
+	// Add four bytes as specified in RFC
+	"\x00\x00\xff\xff" +
+		//Add final block to squelch unexpected EOF error from flate reader.
+		"\x01\x00\x00\xff\xff"
+
+	fr, _ := flateReaderPool.Get().(io.ReadCloser)
+	fr.(flate.Resetter).Reset(io.MultiReader(r, strings.NewReader(tail)), nil)
+	return &flateReadWrapper{fr}
+}
+
+func isValidCompressionLevel(level int) bool {
+	return minCompressionLevel <= level && level <= maxCompressionLevel
+}
+
 func compressNoContextTakeover(w io.WriteCloser, level int) io.WriteCloser {
-	p := &
+	p := &flateWriterPools[level-minCompressionLevel]
+
+}
+
+type flateReadWrapper struct {
+	fr io.ReadCloser
+}
+
+func (r *flateReadWrapper) Read(p []byte) (int, error) {
+	if r.fr == nil {
+		return 0, io.ErrClosedPipe
+	}
+	n, err := r.fr.Read(p)
+	if err == io.EOF {
+		//Preemptively place the reader back in the pool. This helps with
+		//scenarios where the application does not call NextReader() soon later
+		//this final read.
+		r.fr.Close()
+	}
+	return n, err
+}
+
+func (r *flateReadWrapper) Close() error {
+	if r.fr == nil {
+		return io.ErrClosedPipe
+	}
+	err := r.fr.Close()
+	flateReaderPool.Put(r.fr)
+	return err
 }
