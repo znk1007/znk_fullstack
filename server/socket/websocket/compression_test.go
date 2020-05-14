0bdd952db77c062f06d5743a156d002361a65024
znk1007 <1032479745@qq.com>
2020-05-14 22:16:58 +0800

    0.0.4

---
+++ server/socket/websocket/compression_test.go
@@ -0,0 +1,18 @@
+package ws
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+type nopCloser struct{ io.Writer }
+
+func (nopCloser) Close() error { return nil }
+
+func TestTruncWriter(t *testing.T) {
+	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
+	for n := 1; n <= 10; n++ {
+		var b bytes.Buffer
+	}
+}
