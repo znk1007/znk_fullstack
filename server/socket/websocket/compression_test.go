80d42dbf90aa0806e61d6130476b32096bacc17f
znk1007 <1032479745@qq.com>
2020-05-14 22:19:42 +0800

    0.0.4

--- server/socket/websocket/compression_test.go
+++ server/socket/websocket/compression_test.go
@@ -14,5 +14,19 @@ func TestTruncWriter(t *testing.T) {
 	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
 		var b bytes.Buffer
+		w := &truncWriter{w: nopCloser{&b}}
+		p := []byte(data)
+		for len(p) > 0 {
+			m := len(p)
+			if m > n {
+				m = n
+			}
+			w.Write(p[:m])
+			p = p[m:]
+		}
+		if b.String() != data[:len(data)-len(w.p)] {
+			t.Errorf("%d: %q", n, b.String())
+		}
 	}
+
 }
