ee2d833e1c6ea0cea043d65c9555fd5bd736e1e0
znk1007 <1032479745@qq.com>
2020-05-20 18:27:27 +0800

    0.0.4

--- server/socket/websocket/example_test.go
+++ server/socket/websocket/example_test.go
@@ -1,7 +1,9 @@
 package ws_test
 
 import (
+	"log"
 	"net/http"
+	"testing"
 
 	ws "github.com/znk_fullstack/server/socket/websocket"
 )
@@ -20,5 +22,20 @@ var (
 //	ws.CloseGoingAway. All other close messages are likely the result of an
 //	application or protocol error and are logged to aid debugging.
 func ExampleIsUnexpectedCloseError() {
-
+	for {
+		messageType, p, err := c.ReadMessage()
+		if err != nil {
+			if ws.IsUnexpectedCloseError(err, ws.CloseGoingAway) {
+				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
+			}
+			return
+		}
+		processMessage(messageType, p)
+	}
 }
+
+func processMessage(mt int, p []byte) {}
+
+// 	TestX prevents godoc from showing this entire file in the example.
+//	Remove this function when a second example is added.
+func TestX(t *testing.T) {}
