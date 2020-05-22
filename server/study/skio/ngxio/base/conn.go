3b36d7a9df392049ed214bf094cfd60c2f059009
znk1007 <1032479745@qq.com>
2020-05-22 15:44:53 +0800

    0.0.4

---
+++ server/study/skio/ngxio/base/conn.go
@@ -0,0 +1,91 @@
+package base
+
+import (
+	"encoding/json"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+var chars = []byte("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_")
+
+//Timestamp returns a string based on different nano time.
+func Timestamp() string {
+	now := time.Now().UnixNano()
+	ret := make([]byte, 0, 16)
+	for now > 0 {
+		ret = append(ret, chars[int(now%int64(len(chars)))])
+		now = now / int64(len(chars))
+	}
+	return string(ret)
+}
+
+//Conn connection interface
+type Conn interface {
+	FrameReader
+	FrameWriter
+	io.Closer
+	URL() url.URL
+	LocalAddr() net.Addr
+	RemoteAddr() net.Addr
+	RemoteHeader() http.Header
+	SetReadDeadline(t time.Time) error
+	SetWriteDeadline(t time.Time) error
+}
+
+//ConnParamters is connection parameter of server
+type ConnParamters struct {
+	PingInterval time.Duration
+	PingTimeout  time.Duration
+	SID          string
+	Upgrades     []string
+}
+
+type jsonParameters struct {
+	SID          string   `json:"sid"`
+	Upgrades     []string `json:"upgrades"`
+	PingInterval int      `json:"pingInterval"`
+	PingTimeout  int      `json:"pingTimeout"`
+}
+
+//ReadConnParameters reads ConnParameters from r.
+func ReadConnParameters(r io.Reader) (ConnParamters, error) {
+	var param jsonParameters
+	if err := json.NewDecoder(r).Decode(&param); err != nil {
+		return ConnParamters{}, err
+	}
+	return ConnParamters{
+		SID:          param.SID,
+		Upgrades:     param.Upgrades,
+		PingInterval: time.Duration(param.PingInterval) * time.Millisecond,
+		PingTimeout:  time.Duration(param.PingTimeout) * time.Millisecond,
+	}, nil
+}
+
+//WriteTo writes to w with json format.
+func (cp ConnParamters) WriteTo(w io.Writer) (int64, error) {
+	arg := jsonParameters{
+		SID:          cp.SID,
+		Upgrades:     cp.Upgrades,
+		PingInterval: int(cp.PingInterval / time.Millisecond),
+		PingTimeout:  int(cp.PingTimeout / time.Millisecond),
+	}
+	writer := writer{
+		w: w,
+	}
+	err := json.NewEncoder(&writer).Encode(arg)
+	return writer.i, err
+}
+
+type writer struct {
+	i int64
+	w io.Writer
+}
+
+func (w *writer) Write(p []byte) (int, error) {
+	n, err := w.w.Write(p)
+	w.i += int64(n)
+	return n, err
+}
