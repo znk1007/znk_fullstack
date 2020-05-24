3649ee5281d53d6e7422e9513c664cfe14537819
znk1007 <1032479745@qq.com>
2020-05-24 22:23:53 +0800

    0.0.4

--- server/study/skio/ngxio/payload/encoder_test.go
+++ server/study/skio/ngxio/payload/encoder_test.go
@@ -2,11 +2,15 @@ package payload
 
 import (
 	"bytes"
+	"errors"
 	"io"
+	"io/ioutil"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/znk_fullstack/server/study/skio/ngxio/base"
 )
 
 type fakeWriterFeeder struct {
@@ -50,5 +54,155 @@ func TestEncoder(t *testing.T) {
 			must.Nil(err)
 		}
 		at.Equal(test.data, buf.Bytes())
+	}
+}
+
+func TestEncoderBeginError(t *testing.T) {
+	at := assert.New(t)
+	buf := bytes.NewBuffer(nil)
+	f := &fakeWriterFeeder{
+		w: buf,
+	}
+	e := encoder{
+		supportBinary: true,
+		feeder:        f,
+	}
+	buf.Reset()
+	targetErr := newOpError("payload", errPaused)
+	f.returnError = targetErr
+
+	_, err := e.NextWriter(base.FrameBinary, base.OPEN)
+	at.Equal(targetErr, err)
+}
+
+type errorWrite struct {
+	err error
+}
+
+func (f *errorWrite) Write(p []byte) (int, error) {
+	return 0, f.err
+}
+
+func TestEncoderEndError(t *testing.T) {
+	at := assert.New(t)
+	must := require.New(t)
+	werr := errors.New("write error")
+	f := &fakeWriterFeeder{
+		w: &errorWrite{
+			err: werr,
+		},
+	}
+	e := encoder{
+		supportBinary: true,
+		feeder:        f,
+	}
+
+	targetErr := errors.New("error")
+
+	fw, err := e.NextWriter(base.FrameBinary, base.OPEN)
+	must.Nil(err)
+	f.returnError = targetErr
+	err = fw.Close()
+	at.Equal(targetErr, err)
+	at.Equal(f.passinErr, werr)
+}
+
+func TestEncoderNOOP(t *testing.T) {
+	at := assert.New(t)
+
+	tests := []struct {
+		supportBinary bool
+		data          []byte
+	}{
+		{true, []byte{0x00, 0x01, 0xff, '6'}},
+		{false, []byte("1:6")},
+	}
+
+	for _, test := range tests {
+		e := encoder{
+			supportBinary: test.supportBinary,
+		}
+		at.Equal(test.data, e.NOOP())
+	}
+
+	//NOOP should be thread-safe
+	var wg sync.WaitGroup
+	max := 100
+	wg.Add(100)
+	for i := 0; i < max; i++ {
+		go func(i int) {
+			defer wg.Done()
+			e := encoder{
+				supportBinary: i&0x1 == 0,
+			}
+			e.NOOP()
+		}(i)
+	}
+	wg.Wait()
+}
+
+func BenchmarkStringEncoder(b *testing.B) {
+	must := require.New(b)
+	packets := []Packet{
+		{base.FrameString, base.OPEN, []byte{}},
+		{base.FrameString, base.MESSAGE, []byte("你好\n")},
+		{base.FrameString, base.PING, []byte("probe")},
+	}
+	e := encoder{
+		supportBinary: false,
+		feeder: &fakeWriterFeeder{
+			w: ioutil.Discard,
+		},
+	}
+
+	//warm up for memory allocation
+	for _, p := range packets {
+		f, err := e.NextWriter(p.ft, p.pt)
+		must.Nil(err)
+		_, err = f.Write(p.data)
+		must.Nil(err)
+		err = f.Close()
+		must.Nil(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, p := range packets {
+			f, _ := e.NextWriter(p.ft, p.pt)
+			f.Write(p.data)
+			f.Close()
+		}
+	}
+}
+
+func BenchmarkB64Encoder(b *testing.B) {
+	must := require.New(b)
+	packets := []Packet{
+		{base.FrameBinary, base.OPEN, []byte{}},
+		{base.FrameBinary, base.MESSAGE, []byte("你好\n")},
+		{base.FrameBinary, base.PING, []byte("probe")},
+	}
+	e := encoder{
+		supportBinary: false,
+		feeder: &fakeWriterFeeder{
+			w: ioutil.Discard,
+		},
+	}
+
+	//warm up for memory allocation
+	for _, p := range packets {
+		f, err := e.NextWriter(p.ft, p.pt)
+		must.Nil(err)
+		_, err = f.Write(p.data)
+		must.Nil(err)
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, p := range packets {
+			f, _ := e.NextWriter(p.ft, p.pt)
+			f.Write(p.data)
+			f.Close()
+		}
 	}
 }
