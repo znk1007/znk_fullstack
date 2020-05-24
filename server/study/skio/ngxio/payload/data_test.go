8b9584b36f70a5d36fd1fd5e143412d2ee825aa1
znk1007 <1032479745@qq.com>
2020-05-24 16:25:38 +0800

    0.0.4

--- server/study/skio/ngxio/payload/data_test.go
+++ server/study/skio/ngxio/payload/data_test.go
@@ -20,4 +20,61 @@ var tests = []struct {
 			{base.FrameString, base.OPEN, []byte{}},
 		},
 	},
+	{
+		true,
+		[]byte{0x00, 0x01, 0x03, 0xff, '4', 'h', 'e', 'l', 'l', 'o', ' ', 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd},
+		[]Packet{
+			{base.FrameString, base.MESSAGE, []byte("hello 你好")},
+		},
+	},
+	{
+		true,
+		[]byte{0x01, 0x01, 0x03, 0xff, 0x04, 'h', 'e', 'l', 'l', 'o', ' ', 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd},
+		[]Packet{
+			{base.FrameBinary, base.MESSAGE, []byte("hello 你好")},
+		},
+	},
+	{
+		true,
+		[]byte{
+			0x01, 0x07, 0xff, 0x04, 'h', 'e', 'l', 'l', 'o', '\n',
+			0x00, 0x08, 0xff, '4', 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd, '\n',
+			0x00, 0x06, 0xff, '2', 'p', 'r', 'o', 'b', 'e',
+		},
+		[]Packet{
+			{base.FrameBinary, base.MESSAGE, []byte("hello\n")},
+			{base.FrameString, base.MESSAGE, []byte("你好\n")},
+			{base.FrameString, base.PING, []byte("probe")},
+		},
+	},
+	{
+		false,
+		[]byte("1:0"),
+		[]Packet{
+			{base.FrameString, base.OPEN, []byte{}},
+		},
+	},
+	{
+		false,
+		[]byte("13:4hello 你好"),
+		[]Packet{
+			{base.FrameString, base.MESSAGE, []byte("hello 你好")},
+		},
+	},
+	{
+		false,
+		[]byte("18:b4aGVsbG8g5L2g5aW9"),
+		[]Packet{
+			{base.FrameBinary, base.MESSAGE, []byte("hello 你好")},
+		},
+	},
+	{
+		false,
+		[]byte("10:b4aGVsbG8K8:4你好\n6:2probe"),
+		[]Packet{
+			{base.FrameBinary, base.MESSAGE, []byte("hello\n")},
+			{base.FrameString, base.MESSAGE, []byte("你好\n")},
+			{base.FrameString, base.PING, []byte("probe")},
+		},
+	},
 }
--- server/study/skio/ngxio/payload/decoder_test.go
+++ server/study/skio/ngxio/payload/decoder_test.go
@@ -3,6 +3,7 @@ package payload
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -42,6 +43,34 @@ func TestDecoder(t *testing.T) {
 	must := require.New(t)
 
 	for _, test := range tests {
+		feeder := fakeReaderFeeder{
+			data:          test.data,
+			supportBinary: test.supportBinary,
+		}
+		d := decoder{
+			feeder: &feeder,
+		}
+		var packets []Packet
 
+		for i := 0; i < len(test.packets); i++ {
+			ft, pt, fr, err := d.NextReader()
+			if err != nil {
+				must.Equal(io.EOF, err)
+				break
+			}
+			data, err := ioutil.ReadAll(fr)
+			must.Nil(err)
+			packet := Packet{
+				ft:   ft,
+				pt:   pt,
+				data: data,
+			}
+			err = fr.Close()
+			must.Nil(err)
+			packets = append(packets, packet)
+		}
+		at.Equal(test.packets, packets)
+		at.Equal(feeder.getCounter, 1)
+		at.Equal(feeder.setCounter, 1)
 	}
 }
