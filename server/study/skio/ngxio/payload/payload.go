99b7d9c623e15d299b7496d7dc45a20cfedb1670
znk1007 <1032479745@qq.com>
2020-05-25 14:32:46 +0800

    0.0.4

--- server/study/skio/ngxio/payload/payload.go
+++ server/study/skio/ngxio/payload/payload.go
@@ -1,1 +1,251 @@
 package payload
+
+import (
+	"io"
+	"math"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/znk_fullstack/server/study/skio/ngxio/base"
+)
+
+type readArg struct {
+	r             io.Reader
+	supportBinary bool
+}
+
+//Payload does encode or decode to payload protocol
+type Payload struct {
+	close     chan struct{}
+	closeOnce sync.Once
+	err       atomic.Value
+
+	pauser *pauser
+
+	readerChan   chan readArg
+	feeding      int32
+	readError    chan error
+	readDeadline atomic.Value
+	decoder      decoder
+
+	writerChan    chan io.Writer
+	flushing      int32
+	writeError    chan error
+	writeDeadline atomic.Value
+	encoder       encoder
+}
+
+//New returns a new payload
+func New(supportBinary bool) *Payload {
+	ret := &Payload{
+		close:      make(chan struct{}),
+		pauser:     newPauser(),
+		readerChan: make(chan readArg),
+		readError:  make(chan error),
+		writerChan: make(chan io.Writer),
+		writeError: make(chan error),
+	}
+	ret.readDeadline.Store(time.Time{})
+	ret.decoder.feeder = ret
+	ret.writeDeadline.Store(time.Time{})
+	ret.encoder.supportBinary = supportBinary
+	ret.encoder.feeder = ret
+	return ret
+}
+
+//NextWriter returns a writer for next frame.
+//NextWriter and SetWriteDeadline needs be called sync.
+//NextWriter will wait a FlushOut call, the it returns WriteCloser which
+//encode package to FlushOut's Writer.
+//
+//If Close called when NextWriter, it returns io.EOF.
+//If beyond the time set by SetWriteDeadline, it returns ErrTimeout.
+
+func (p *Payload) NextWriter(ft base.FrameType, pt base.PacketType) (io.WriteCloser, error) {
+	return p.encoder.NextWriter(ft, pt)
+}
+
+//SetWriteDeadline sets next writer deadline.
+//NextWriter and SetWriteDeadline, it return io.EOF.
+//
+//If Close called when SetWriteDeadline, it return io.EOF.
+//Pause doesn't effect to SetWriteDeadline.
+func (p *Payload) SetWriteDeadline(t time.Time) error {
+	p.writeDeadline.Store(t)
+	return nil
+}
+
+//Pause pauses the payload. It will wait all reader and writer closed
+//which created from NextReader or NextWriter.
+//It can call in multi-goroutine.
+func (p *Payload) Pause() {
+	p.pauser.Pause()
+}
+
+//Resume resumes the payload.
+//It can call in multi-goroutine.
+func (p *Payload) Resume() {
+	p.pauser.Resume()
+}
+
+//Close closes the payload.
+//It can call in multi-goroutine.
+func (p *Payload) Close() error {
+	p.closeOnce.Do(func() {
+		close(p.close)
+	})
+	return nil
+}
+
+//Store stores a error in payload, and block all other request.
+func (p *Payload) Store(op string, err error) error {
+	old := p.err.Load()
+	if old == nil {
+		if err == io.EOF || err == nil {
+			return err
+		}
+		op := newOpError(op, err)
+		p.err.Store(op)
+		return op
+	}
+	return old.(error)
+}
+
+func (p *Payload) setReader(err error) error {
+	select {
+	case <-p.close:
+		return p.load()
+	default:
+	}
+	for {
+		after, ok := p.readTimeout()
+		if !ok {
+			return p.Store("read", errTimeout)
+		}
+		select {
+		case <-p.close:
+			return p.load()
+		case <-after:
+			continue
+		case p.readError <- err:
+		}
+		return nil
+	}
+}
+
+func (p *Payload) getReader() (io.Reader, bool, error) {
+	select {
+	case <-p.close:
+		return nil, false, p.load()
+	default:
+	}
+
+	if ok := p.pauser.Working(); !ok {
+		return nil, false, newOpError("payload", errPaused)
+	}
+	p.pauser.Done()
+
+	for {
+		after, ok := p.readTimeout()
+		if !ok {
+			return nil, false, p.Store("read", errTimeout)
+		}
+		select {
+		case <-p.close:
+			return nil, false, p.load()
+		case <-p.pauser.PausedTrigger():
+			return nil, false, newOpError("payload", errPaused)
+		case <-after:
+			continue
+		case arg := <-p.readerChan:
+			return arg.r, arg.supportBinary, nil
+		}
+	}
+}
+
+func (p *Payload) getWriter() (io.Writer, error) {
+	select {
+	case <-p.close:
+		return nil, p.load()
+	default:
+	}
+	if ok := p.pauser.Working(); !ok {
+		return nil, newOpError("payload", errPaused)
+	}
+	p.pauser.Done()
+
+	for {
+		after, ok := p.writeTimeout()
+		if !ok {
+			return nil, p.Store("write", errPaused)
+		}
+		select {
+		case <-p.close:
+			return nil, p.load()
+		case <-p.pauser.PausedTrigger():
+			return nil, newOpError("payload", errPaused)
+		case <-after:
+			continue
+		case w := <-p.writerChan:
+			return w, nil
+		}
+	}
+}
+
+func (p *Payload) setWriter(err error) error {
+	select {
+	case <-p.close:
+		return p.load()
+	default:
+	}
+	for {
+		after, ok := p.writeTimeout()
+		if !ok {
+			return p.Store("write", errTimeout)
+		}
+		ret := p.Store("write", err)
+		select {
+		case <-p.close:
+			return p.load()
+		case <-after:
+			continue
+		case p.writeError <- err:
+			return ret
+		}
+	}
+}
+
+func (p *Payload) readTimeout() (<-chan time.Time, bool) {
+	deadline := p.readDeadline.Load().(time.Time)
+	wait := deadline.Sub(time.Now())
+	if deadline.IsZero() {
+		//wait for every
+		wait = math.MaxInt64
+	}
+	if wait <= 0 {
+		return nil, false
+	}
+	return time.After(wait), true
+}
+
+func (p *Payload) writeTimeout() (<-chan time.Time, bool) {
+	deadline := p.writeDeadline.Load().(time.Time)
+	wait := deadline.Sub(time.Now())
+	if deadline.IsZero() {
+		//wait for every
+		wait = math.MaxInt64
+	}
+	if wait <= 0 {
+		return nil, false
+	}
+	return time.After(wait), true
+}
+
+func (p *Payload) load() error {
+	ret := p.err.Load()
+	if ret == nil {
+		return io.EOF
+	}
+	return ret.(error)
+}
