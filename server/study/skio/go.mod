720a3a1b8ee9d10e59fa712b6d3cfa654b0b74bd
znk1007 <1032479745@qq.com>
2020-05-25 10:44:29 +0800

    0.0.4

--- server/study/skio/go.mod
+++ server/study/skio/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/containerd/containerd v1.3.4 // indirect
 	github.com/google/go-cmp v0.4.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/smartystreets/assertions v1.1.0 // indirect
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/text v0.3.2 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
--- server/study/skio/ngxio/payload/pauser_test.go
+++ server/study/skio/ngxio/payload/pauser_test.go
@@ -1,7 +1,9 @@
 package payload
 
 import (
+	"math/rand"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -58,4 +60,199 @@ func TestPauserTrigger(t *testing.T) {
 		should.True(false, "should not run here")
 	case <-time.After(time.Second / 10):
 	}
+}
+
+func TestPauserPauseOnlyOnce(t *testing.T) {
+	at := assert.New(t)
+	p := newPauser()
+	s := make(chan int)
+
+	go func() {
+		ok := p.Pause()
+		at.True(ok)
+		defer p.Resume()
+		s <- 1
+		<-s
+	}()
+	<-s
+	ok := p.Pause()
+	at.False(ok)
+	s <- 1
+}
+
+func TestPauserPauseAfterResume(t *testing.T) {
+	should := assert.New(t)
+	p := newPauser()
+
+	ok := p.Pause()
+	should.True(ok)
+	p.Resume()
+
+	ok = p.Pause()
+	should.True(ok)
+	p.Resume()
+}
+
+func TestPauserPauseMultiplyREsumeOnce(t *testing.T) {
+	should := assert.New(t)
+	p := newPauser()
+
+	ok := p.Pause()
+	should.True(ok)
+	for i := 0; i < 10; i++ {
+		ok = p.Pause()
+		should.False(ok)
+	}
+	p.Resume()
+
+	//check if it reset to normal
+	ok = p.Pause()
+	should.True(ok)
+	p.Resume()
+}
+
+func TestPauserConcurrencyWorkingDone(t *testing.T) {
+	p := newPauser()
+	wg := sync.WaitGroup{}
+	f := func() {
+		defer wg.Done()
+		should := assert.New(t)
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		time.Sleep(time.Microsecond * time.Duration(r.Intn(100)))
+		ok := p.Working()
+		should.True(ok)
+		defer p.Done()
+		time.Sleep(time.Microsecond * time.Duration(r.Intn(100)))
+	}
+	max := 1000
+	wg.Add(max)
+	for i := 0; i < max; i++ {
+		go f()
+	}
+	wg.Wait()
+}
+
+func TestPauserCanWorkingDuringPauseWaiting(t *testing.T) {
+	should := assert.New(t)
+	p := newPauser()
+	wg := sync.WaitGroup{}
+
+	ok := p.Working()
+	should.True(ok)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		o := p.Pause()
+		should.True(o)
+		defer p.Resume()
+	}()
+	<-p.PausingTrigger()
+
+	ok = p.Working()
+	should.True(ok)
+
+	p.Done()
+	p.Done()
+	wg.Wait()
+}
+
+func TestPauserPauseWhenAllDone(t *testing.T) {
+	should := assert.New(t)
+	p := newPauser()
+
+	n := 10
+	for i := 0; i < n; i++ {
+		ok := p.Working()
+		should.True(ok)
+	}
+	for i := 0; i < n; i++ {
+		p.Done()
+	}
+	ok := p.Pause()
+	should.True(ok)
+
+	ok = p.Pause()
+	should.False(ok)
+
+	p.Resume()
+}
+
+func TestPauserOnlyOnePauseAfterWaiting(t *testing.T) {
+	should := assert.New(t)
+	count := int64(0)
+	wg := sync.WaitGroup{}
+	p := newPauser()
+
+	ok := p.Working()
+	should.True(ok)
+
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			ok := p.Pause()
+			if ok {
+				atomic.AddInt64(&count, 1)
+			}
+		}()
+	}
+	time.Sleep(time.Second / 10) //wait all goroutines pausing
+
+	p.Done()
+	wg.Wait()
+	should.Equal(int64(1), count)
+	p.Resume()
+}
+
+func TestPauserCannotWorkingAfterPause(t *testing.T) {
+	should := assert.New(t)
+	p := newPauser()
+
+	ok := p.Pause()
+	should.True(ok)
+	defer p.Resume()
+
+	ok = p.Working()
+	should.False(ok)
+	p.Done()
+}
+
+func TestPauserRandom(t *testing.T) {
+	p := newPauser()
+	wg := sync.WaitGroup{}
+	n := 100
+
+	f := func() {
+		defer wg.Done()
+		should := assert.New(t)
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		time.Sleep(time.Microsecond * time.Duration(r.Intn(n)))
+		ok := p.Working()
+		should.True(ok)
+		defer p.Done()
+		time.Sleep(time.Microsecond * time.Duration(r.Intn(n)))
+	}
+	max := 1000
+	wg.Add(max)
+	for i := 0; i < max; i++ {
+		go f()
+	}
+
+	should := assert.New(t)
+	//Make sure waiting pause.
+	ok := p.Working()
+	should.True(ok)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Microsecond * time.Duration(n/2))
+		p.Done()
+	}()
+	start := time.Now()
+	ok = p.Pause()
+	end := time.Now()
+	should.True(ok)
+	should.True(end.Sub(start) > time.Microsecond)
+	wg.Wait()
 }
