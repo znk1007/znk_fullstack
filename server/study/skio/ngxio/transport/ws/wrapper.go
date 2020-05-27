bea166e494e67a0423a1c841ceaa073de6bc1e30
znk1007 <1032479745@qq.com>
2020-05-27 22:24:56 +0800

    0.0.4

--- server/study/skio/ngxio/transport/ws/wrapper.go
+++ server/study/skio/ngxio/transport/ws/wrapper.go
@@ -1,19 +1,68 @@
 package ws
 
 import (
-	"net/http"
-	"net/url"
+	"fmt"
+	"io"
 	"sync"
+	"time"
 
+	"github.com/znk_fullstack/server/study/skio/ngxio/base"
 	websocket "github.com/znk_fullstack/server/study/skio/ws"
 )
 
 type wrapper struct {
-	url          url.URL
-	remoteHeader http.Header
-	readLocker   *sync.Mutex
+	*websocket.Conn
+	writeLock  *sync.Mutex
+	readLocker *sync.Mutex
 }
 
 func newWrapper(conn *websocket.Conn) wrapper {
+	return wrapper{
+		Conn:       conn,
+		writeLock:  new(sync.Mutex),
+		readLocker: new(sync.Mutex),
+	}
+}
 
+func (w wrapper) NextReader() (base.FrameType, io.ReadCloser, error) {
+	w.readLocker.Lock()
+	t, r, e := w.Conn.NextReader()
+	//The wrapper remains locked until the returned ReadCloser is Closed.
+	if e != nil {
+		w.readLocker.Unlock()
+		return 0, nil, e
+	}
+	switch t {
+	case websocket.TextMessage:
+		return base.FrameString, newRcWrapper(w.readLocker, r), nil
+	case websocket.BinaryMessage:
+		return base.FrameBinary, newRcWrapper(w.readLocker, r), nil
+	}
+	w.readLocker.Unlock()
+	return 0, nil, transport.errin
+}
+
+type rcWrapper struct {
+	nagTime *time.Timer
+	quitNag chan struct{}
+	l       *sync.Mutex
+	io.Reader
+}
+
+func newRcWrapper(l *sync.Mutex, r io.Reader) rcWrapper {
+	timer := time.NewTimer(30 * time.Second)
+	q := make(chan struct{})
+	go func() {
+		select {
+		case <-q:
+		case <-timer.C:
+			fmt.Println("Did you forget to Close() the ReadCloser from NextReader?")
+		}
+	}()
+	return rcWrapper{
+		nagTime: timer,
+		quitNag: q,
+		l:       l,
+		Reader:  r,
+	}
 }
