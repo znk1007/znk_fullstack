dca5f360040b64909c325f1a560fb78b705f814f
znk1007 <1032479745@qq.com>
2020-05-29 10:57:20 +0800

    0.0.4

--- server/study/skio/ngxio/manager.go
+++ server/study/skio/ngxio/manager.go
@@ -1,6 +1,52 @@
 package ngxio
 
+import (
+	"strconv"
+	"sync"
+	"sync/atomic"
+)
+
 //SessionIDGenerator generates new session id.
 type SessionIDGenerator interface {
 	NewID() string
+}
+
+type defaultIDGenrator struct {
+	nextID uint64
+}
+
+func (g *defaultIDGenrator) NewID() string {
+	id := atomic.AddUint64(&g.nextID, 1)
+	return strconv.FormatUint(id, 36)
+}
+
+type manager struct {
+	SessionIDGenerator
+	s      map[string]*session
+	locker sync.RWMutex
+}
+
+func newManager(g SessionIDGenerator) *manager {
+	if g == nil {
+		g = &defaultIDGenrator{}
+	}
+	return &manager{
+		SessionIDGenerator: g,
+		s:                  make(map[string]*session),
+	}
+}
+
+func (m *manager) Add(s *session) {
+	m.locker.RLock()
+	defer m.locker.Unlock()
+	m.s[s.ID()] = s
+}
+
+func (m *manager) Remove(sid string) {
+	m.locker.Lock()
+	defer m.locker.Unlock()
+	if _, ok := m.s[sid]; !ok {
+		return
+	}
+	delete(m.s, sid)
 }
