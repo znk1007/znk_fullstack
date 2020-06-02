af064fec65d467bcd5d02d88dc6afbf3d594cfa7
znk1007 <1032479745@qq.com>
2020-06-02 08:53:03 +0800

    0.0.4

--- server/study/skio/sio/namespace.go
+++ server/study/skio/sio/namespace.go
@@ -1,6 +1,12 @@
 package sio
 
-import "sync"
+import (
+	"errors"
+	"reflect"
+	"sync"
+
+	"github.com/znk_fullstack/server/study/skio/sio/parser"
+)
 
 type namespaceHandler struct {
 	onConnect    func(c Conn) error
@@ -31,6 +37,57 @@ func (nh *namespaceHandler) OnError(f func(Conn, error)) {
 
 func (nh *namespaceHandler) OnEvent(event string, f interface{}) {
 	nh.events[event] = newEventFunc(f)
+}
+
+func (nh *namespaceHandler) getTypes(header parser.Header, event string) []reflect.Type {
+	switch header.Type {
+	case parser.Error:
+		fallthrough
+	case parser.Disconnect:
+		return []reflect.Type{reflect.TypeOf("")}
+	case parser.Event:
+		namespaceHandler := nh.events[event]
+		if namespaceHandler == nil {
+			return nil
+		}
+		return namespaceHandler.argTyps
+	}
+	return nil
+}
+
+func (nh *namespaceHandler) dispatch(c Conn, header parser.Header, event string, args []reflect.Value) ([]reflect.Value, error) {
+	switch header.Type {
+	case parser.Connect:
+		var err error
+		if nh.onConnect != nil {
+			err = nh.onConnect(c)
+		}
+		return nil, err
+	case parser.Disconnect:
+		msg := ""
+		if len(args) > 0 {
+			msg = args[0].Interface().(string)
+		}
+		if nh.onDisconnect != nil {
+			nh.onDisconnect(c, msg)
+		}
+		return nil, nil
+	case parser.Error:
+		msg := ""
+		if len(args) > 0 {
+			msg = args[0].Interface().(string)
+		}
+		if nh.onError != nil {
+			nh.onError(c, errors.New(msg))
+		}
+	case parser.Event:
+		namespaceHandler := nh.events[event]
+		if namespaceHandler == nil {
+			return nil, nil
+		}
+		return namespaceHandler.Call(append([]reflect.Value{reflect.ValueOf(c)}, args...))
+	}
+	return nil, errors.New("invalid packet type")
 }
 
 type namespaceConn struct {
