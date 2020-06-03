e2885541b6f7d52e8abf9f148d9fbfd4ddc0938f
znk1007 <1032479745@qq.com>
2020-06-03 16:16:04 +0800

    0.0.4

--- server/study/skio/sio/server.go
+++ server/study/skio/sio/server.go
@@ -29,4 +29,46 @@ func (s *Server) Close() error {
 	return s.eio.Close()
 }
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.eio.ServeHTTP(w, r)
+}
+
+//OnConnect set a handler function f
+//to handle open event for namespace nsp
+func (s *Server) OnConnect(nsp string, f func(Conn) error) {
+	h := s.getNamespace(nsp, true)
+	h.OnConnect(f)
+}
+
+//OnDisconnect set a handler function f to handle open event
+//for namespace nsp.
+func (s *Server) OnDisconnect(nsp string, f func(Conn, string)) {
+
+}
+
+func (s *Server) serveConn(c ngxio.Conn) {
+	_, err := newConn(c, s.handlers)
+	if err != nil {
+		root := s.handlers[""]
+		if root != nil && root.onError != nil {
+			root.onError(nil, err)
+		}
+		return
+	}
+}
+
+func (s *Server) getNamespace(nsp string, create bool) *namespaceHandler {
+	if nsp == "/" {
+		nsp = ""
+	}
+	ret, ok := s.handlers[nsp]
+	if ok {
+		return ret
+	}
+	if create {
+		handler := newHandler()
+		s.handlers[nsp] = handler
+		return handler
+	}
+	return nil
+}
