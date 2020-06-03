2d7c1da29be4fb49c22a9d0d1c4e3c14a039cc49
znk1007 <1032479745@qq.com>
2020-06-03 17:14:29 +0800

    0.0.4

--- server/study/skio/sio/server.go
+++ server/study/skio/sio/server.go
@@ -35,15 +35,119 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 //OnConnect set a handler function f
 //to handle open event for namespace nsp
-func (s *Server) OnConnect(nsp string, f func(Conn) error) {
-	h := s.getNamespace(nsp, true)
+func (s *Server) OnConnect(namespace string, f func(Conn) error) {
+	h := s.getNamespace(namespace, true)
 	h.OnConnect(f)
 }
 
 //OnDisconnect set a handler function f to handle open event
 //for namespace nsp.
-func (s *Server) OnDisconnect(nsp string, f func(Conn, string)) {
+func (s *Server) OnDisconnect(namespace string, f func(Conn, string)) {
+	h := s.getNamespace(namespace, true)
+	h.OnDisconnect(f)
+}
 
+//OnError set a handler function f to
+//handle error for namespace nsp.
+func (s *Server) OnError(namespace string, f func(Conn, error)) {
+	h := s.getNamespace(namespace, true)
+	h.OnError(f)
+}
+
+//OnEvent set a handler function f to
+//handle event for namespace nsp.
+func (s *Server) OnEvent(namespace, event string, f interface{}) {
+	h := s.getNamespace(namespace, true)
+	h.OnEvent(event, f)
+}
+
+//Serve serves socket server
+func (s *Server) Serve() error {
+	for {
+		conn, err := s.eio.Accept()
+		if err != nil {
+			return err
+		}
+		go s.serveConn(conn)
+	}
+}
+
+//JoinRoom joins given connection to the room
+func (s *Server) JoinRoom(namespace, room string, connection Conn) bool {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		nspHandler.broadcast.Join(room, connection)
+		return true
+	}
+	return false
+}
+
+//LeaveRoom leaves given connection from the room
+func (s *Server) LeaveRoom(namespace, room string, connection Conn) bool {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		nspHandler.broadcast.Leave(room, connection)
+		return true
+	}
+	return false
+}
+
+//LeaveAllRooms leaves given connection from all rooms
+func (s *Server) LeaveAllRooms(namespace string, connection Conn) bool {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		nspHandler.broadcast.LeaveAll(connection)
+		return true
+	}
+	return false
+}
+
+//ClearRoom clears the room
+func (s *Server) ClearRoom(namespace string, room string) bool {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		nspHandler.broadcast.Clear(room)
+		return true
+	}
+	return false
+}
+
+//BroadcastToRoom broadcasts given event & args to all the connections in the room
+func (s *Server) BroadcastToRoom(namespace, room, event string, args ...interface{}) bool {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		nspHandler.broadcast.Send(room, event, args...)
+		return true
+	}
+	return false
+}
+
+//RoomLen gives number of connections in the room
+func (s *Server) RoomLen(namespace, room string) int {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		return nspHandler.broadcast.Len(room)
+	}
+	return -1
+}
+
+//Rooms gives list of all the rooms
+func (s *Server) Rooms(namespace string) []string {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		return nspHandler.broadcast.Rooms(nil)
+	}
+	return nil
+}
+
+//ForEach map room connections
+func (s *Server) ForEach(namespace, room string, f EachFunc) bool {
+	nspHandler := s.getNamespace(namespace, false)
+	if nspHandler != nil {
+		nspHandler.broadcast.ForEach(room, f)
+		return true
+	}
+	return false
 }
 
 func (s *Server) serveConn(c ngxio.Conn) {
