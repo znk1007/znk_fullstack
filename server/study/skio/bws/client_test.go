71ed068898107d56fe0d53cf3d78637993733f4d
znk1007 <1032479745@qq.com>
2020-06-04 16:45:10 +0800

    0.0.4

---
+++ server/study/skio/bws/client_test.go
@@ -0,0 +1,13 @@
+package bws
+
+import "net/url"
+
+var hostPortNoPortTests = []struct {
+	u                    *url.URL
+	hostPort, hostNoPort string
+}{
+	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
+	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+}
