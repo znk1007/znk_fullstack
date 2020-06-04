83d372e9c4dc32f1153f6c8c5c5021089a492172
znk1007 <1032479745@qq.com>
2020-06-04 17:46:23 +0800

    0.0.4

--- server/study/skio/bws/client_test.go
+++ server/study/skio/bws/client_test.go
@@ -1,6 +1,9 @@
 package bws
 
-import "net/url"
+import (
+	"net/url"
+	"testing"
+)
 
 var hostPortNoPortTests = []struct {
 	u                    *url.URL
@@ -10,4 +13,16 @@ var hostPortNoPortTests = []struct {
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+}
+
+func TestHostPortNoPort(t *testing.T) {
+	for _, tt := range hostPortNoPortTests {
+		hostPort, hostNoPort := hostPortNoPort(tt.u)
+		if hostPort != tt.hostPort {
+			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
+		}
+		if hostNoPort != tt.hostNoPort {
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
+		}
+	}
 }
