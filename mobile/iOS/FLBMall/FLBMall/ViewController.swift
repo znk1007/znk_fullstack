e44d9112e30305e6610ce1efe154a9212719b392
znk1007 <1032479745@qq.com>
2020-06-24 14:54:56 +0800

    flb

---
+++ mobile/iOS/FLBMall/FLBMall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLBMall
+//
+//  Created by Sam Huang on 2020/6/24.
+//  Copyright © 2020 Sam Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
