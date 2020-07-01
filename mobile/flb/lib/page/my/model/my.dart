d33d5585d38839b7f7efdc6ea10def76691ad9d0
znk1007 <1032479745@qq.com>
2020-07-01 22:52:26 +0800

    flb

--- mobile/flb/lib/page/my/model/my.dart
+++ mobile/flb/lib/page/my/model/my.dart
@@ -2,9 +2,39 @@ import 'package:flb/model/user/user.dart';
 import 'package:flb/util/db/protos/generated/user/user.pb.dart';
 import 'package:flutter/widgets.dart';
 
+enum MyListType {
+  extend,//推广
+  cashcoupon,//代金券
+  order,//订单
+  collection,//收藏
+  integ,//积分商城
+  addr,//地址
+  msg,//消息
+  setting,//设置
+  about,//关于
+}
+
+enum MyModelType {
+  normal,//普通
+  system,//系统
+}
+
 class MyModel {
+  //段类型
+  MyModelType type;
   //列表
   List<MyList> lists;
+}
+
+
+
+class MyList {
+  //icon地址
+  String iconPath = '';
+  //标题
+  String title;
+  //列表类型
+  MyListType type;
 }
 
 class MyEquality extends ChangeNotifier {
@@ -14,21 +44,12 @@ class MyEquality extends ChangeNotifier {
   String title;
 }
 
-class MyList {
-  //唯一标识
-  String identifier;
-  //icon地址
-  String iconPath;
-  //标题
-  String title;
-}
-
 class MyModelHandler extends ChangeNotifier {
   List<MyEquality> _equalitys;
 
   //权益数据
   List<MyEquality> get equalitys => _equalitys;
-
+  /* 拉取收益数据 */
   Future<void> fetchEqualityData(UserModel userModel) async {
     List<MyEquality> eqs = [];
     MyEquality eq = MyEquality();
@@ -45,16 +66,82 @@ class MyModelHandler extends ChangeNotifier {
   }
 
   //已登录数据
-  MyModel _myLoginedModel;
+  List<MyModel> _loginedList;
   //未登录数据
-  MyModel _myUnLoginedModel;
+  List<MyModel> _unloginedList;
   //读取我的页面数据
-  MyModel fetchMyList(bool isLogined) {
+  List<MyModel> fetchMyList(bool isLogined) {
     if (isLogined) {
-      if (_myLoginedModel != null) {}
-      return _myLoginedModel;
+      _unloginedList = [];
+      if (_loginedList.length == 0) {
+        List<MyModel> models = [];
+        MyModel model = MyModel();
+        model.type = MyModelType.normal;
+        List<MyList> temp = [];
+        MyList sub = MyList();
+        sub.title = '推广';
+        sub.type = MyListType.extend;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '代金券';
+        sub.type = MyListType.cashcoupon;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '我的订单';
+        sub.type = MyListType.order;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '收藏';
+        sub.type = MyListType.collection;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '积分商城';
+        sub.type = MyListType.integ;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '我的地址';
+        sub.type = MyListType.addr;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '我的消息';
+        sub.type = MyListType.extend;
+        temp.add(sub);
+        sub = MyList();
+        model.lists = temp;
+        models.add(model);
+
+        model = MyModel();
+        temp = [];
+        sub = MyList();
+        sub.title = '关于我们';
+        sub.type = MyListType.about;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '设置';
+        sub.type = MyListType.setting;
+        temp.add(sub);
+        models.add(model);
+        _loginedList = models;
+      }
+      return _loginedList;
     } else {
-      return _myUnLoginedModel;
+      _loginedList = [];
+      if (_unloginedList.length == 0) {
+        List<MyModel> models = [];
+        MyModel model = MyModel();
+        List<MyList> temp = [];
+        MyList sub = MyList();
+        sub.title = '关于我们';
+        sub.type = MyListType.about;
+        temp.add(sub);
+        sub = MyList();
+        sub.title = '设置';
+        sub.type = MyListType.setting;
+        temp.add(sub);
+        models.add(model);
+        _unloginedList = models;
+      }
+      return _unloginedList;
     }
   }
 }
