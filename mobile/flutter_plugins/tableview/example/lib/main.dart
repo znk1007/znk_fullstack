d3a76f08a7d2807f0fe8383ebdb00c021ea3873f
znk1007 <1032479745@qq.com>
2020-07-04 22:33:01 +0800

    flb

--- mobile/flutter_plugins/tableview/example/lib/main.dart
+++ mobile/flutter_plugins/tableview/example/lib/main.dart
@@ -1,58 +1,118 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:tableview/tableview.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Tableview.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: Text('Grouped List View Example'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: ZNKTable(
+          numberOfSection: 50,
         ),
       ),
     );
   }
 }
+
+
+// void main() {
+//   runApp(new MaterialApp(
+//     title: 'Container demo',
+//     home: new MyScrv(),
+//   ));
+// }
+
+// class MyScrv extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+
+//     return new MyScrvState();
+//   }
+// }
+
+// class MyScrvState extends State<MyScrv> {
+//   List<String> _list = new List();
+//   List<Color> myColors = new List();
+
+//   @override
+//   void initState() {
+//     _list.add("政府");
+//     _list.add("部门11");
+//     _list.add("部门22");
+//     myColors.add(Colors.red);
+//     myColors.add(Colors.lightBlue);
+//     myColors.add(Colors.lightBlue);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return new CustomScrollView(physics: ScrollPhysics(), slivers: <Widget>[
+//       const SliverAppBar(
+//         pinned: true,
+//         expandedHeight: 250.0,
+//         flexibleSpace: const FlexibleSpaceBar(
+//           title: const Text('demo'),
+//         ),
+//       ),
+//       new SliverGrid(
+//         gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+//           ///设置item的最大像素宽度  比如 130
+//           maxCrossAxisExtent: 150,
+
+//           ///沿主轴的每个子节点之间的逻辑像素数。 默认垂直方向的子child间距  这里的是主轴方向 当你改变 scrollDirection: Axis.vertical,就是改变了主轴发方向
+//           mainAxisSpacing: 10.0,
+
+//           ///沿横轴的每个子节点之间的逻辑像素数。默认水平方向的子child间距
+//           crossAxisSpacing: 10.0,
+
+//           ///每个孩子的横轴与主轴范围的比率。 child的宽高比  常用来处理child的适配
+//           childAspectRatio: 1.0,
+//         ),
+//         delegate: new SliverChildBuilderDelegate(
+//           (BuildContext context, int index) {
+//             return new Container(
+//               //设置child居中显示
+//               alignment: Alignment.center,
+//               child: Text(
+//                 _list[index],
+//                 style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18.0,
+//                     fontWeight: FontWeight.w500,
+//                     //去掉黄色下划线
+//                     decoration: TextDecoration.none),
+//               ),
+//               decoration: new BoxDecoration(
+//                 color: myColors[index],
+//               ),
+//             );
+//           },
+//           childCount: _list.length,
+//         ),
+//       ),
+//       new SliverFixedExtentList(
+//         itemExtent: 50.0,
+//         delegate: new SliverChildBuilderDelegate(
+//           (BuildContext context, int index) {
+//             return new Container(
+//               alignment: Alignment.center,
+//               color: Colors.lightBlue[100 * (index % 9)],
+//               child: new Text('list item $index'),
+//             );
+//           },
+//         ),
+//       ),
+//     ]);
+//   }
+// }
--- mobile/flutter_plugins/tableview/lib/tableview.dart
+++ mobile/flutter_plugins/tableview/lib/tableview.dart
@@ -1,12 +1,71 @@
+library znktable;
+
 import 'package:flutter/material.dart';
 
+//ZNKTableStyle 表格样式
+enum ZNKTableStyle {
+  plain, //平铺
+  grouped, //分组
+}
+
+//ZNKIndexPath 地址索引
+class ZNKIndexPath {
+  final int section; //段下标
+  final int row; //行下标
+  ZNKIndexPath({this.section, this.row});
+}
+
 class ZNKTable extends StatelessWidget {
-  const ZNKTable({Key key}) : super(key: key);
+  //表格样式
+  final ZNKTableStyle style;
+  //段数
+  final int numberOfSection;
+  //每段行数
+  final int Function(int section) numberOfRowsInSection;
+  //滚轴方向
+  final Axis scrollDirection;
+  //头部视图
+  final List<Widget> Function(BuildContext context, bool innerBoxIsScrolled)
+      headerSliverBuilder;
+  //分割视图
+  final Widget Function(BuildContext context, int index) separatorBuilder;
+
+  //ScrollController 嵌套滚动控制器
+  // ScrollController _nestedScrollCtl;
+
+  ZNKTable(
+      {Key key,
+      this.style = ZNKTableStyle.plain,
+      this.scrollDirection = Axis.vertical,
+      this.numberOfSection = 1,
+      @required this.numberOfRowsInSection,
+      this.separatorBuilder,
+      this.headerSliverBuilder})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    bool test = true;
     return Container(
-      child: child,
+      child: this.headerSliverBuilder != null
+          ? NestedScrollView(
+              headerSliverBuilder: this.headerSliverBuilder, body: null)
+          : ListView.separated(
+              physics: ScrollPhysics(),
+              scrollDirection: this.scrollDirection,
+              itemCount: 50,
+              separatorBuilder: (BuildContext context, int index) {
+                return this.separatorBuilder ?? Container();
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return Text('data $index');
+              },
+            ),
     );
+  }
+
+  List<Widget> _defaultHeaderSliverBuilder(
+      BuildContext context, bool innerBoxIsScrolled) {
+    return <Widget>[SliverAppBar()];
   }
 }
