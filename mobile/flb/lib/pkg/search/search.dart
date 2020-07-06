2c75693991953177d104ce63ae0718996076a115
znk1007 <1032479745@qq.com>
2020-07-06 22:58:25 +0800

    flb

--- mobile/flb/lib/pkg/search/search.dart
+++ mobile/flb/lib/pkg/search/search.dart
@@ -1,3 +1,23 @@
-class Name {
+import 'package:flutter/material.dart';
+
+class SearchStyle {
   
+}
+
+class SearchView extends StatefulWidget {
+  //搜索样式
+  final SearchStyle style;
+  SearchView({Key key,this.style}) : super(key: key);
+
+  @override
+  _SearchViewState createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
 }
