a8d641b464898b693565670e47de4874a5cbc8c4
znk1007 <1032479745@qq.com>
2020-07-07 15:36:21 +0800

    flb

--- mobile/flb/lib/page/home/homepage.dart
+++ mobile/flb/lib/page/home/homepage.dart
@@ -1,5 +1,9 @@
+import 'package:flb/model/style/style.dart';
+import 'package:flb/pkg/screen/screen.dart';
+import 'package:flb/pkg/search/search.dart';
 import 'package:flb/util/random/color.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   static String id = 'home';
@@ -13,11 +17,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: RandomHandler.randomColor,
-      child: Stack(
-        children: [],
-      )
-    );
+    return Consumer<ThemeStyle>(builder: (ctx, style, child) {
+      return Container(
+          child: SearchView(
+              style: SearchStyle(
+                  margin:
+                      EdgeInsets.only(left: 50, top: Screen.safeTopArea + 10),
+                  size: Size(300, 45),
+                  cornerRadius: 45 / 2.0)));
+    });
   }
 }
--- mobile/flb/lib/pkg/search/search.dart
+++ mobile/flb/lib/pkg/search/search.dart
@@ -1,23 +1,66 @@
+import 'package:flb/util/random/color.dart';
 import 'package:flutter/material.dart';
 
 class SearchStyle {
-  
+  //背景颜色
+  Color backgroudColor = Colors.red;
+  //倒角
+  double cornerRadius = 6.0;
+  //大小
+  Size size = Size(200, 60);
+  //边距
+  EdgeInsets margin = EdgeInsets.zero;
+  //默认样式
+  static SearchStyle defaultStyle() => SearchStyle();
+  //初始化
+  SearchStyle(
+      {this.backgroudColor = Colors.red,
+      this.cornerRadius = 6.0,
+      this.size = const Size(200, 60),
+      this.margin = EdgeInsets.zero});
 }
 
 class SearchView extends StatefulWidget {
   //搜索样式
-  final SearchStyle style;
-  SearchView({Key key,this.style}) : super(key: key);
+  SearchStyle _style;
+  SearchView({Key key, SearchStyle style}) {
+    _style = style ?? SearchStyle();
+  }
 
   @override
   _SearchViewState createState() => _SearchViewState();
 }
 
 class _SearchViewState extends State<SearchView> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('height: ${widget._style.size.height}');
     return Container(
-       child: child,
+      margin: widget._style.margin,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(widget._style.cornerRadius),
+          color: widget._style.backgroudColor),
+      width: widget._style.size.width,
+      height: widget._style.size.height,
+      child: Container(
+        width: widget._style.size.width - 10,
+        height: widget._style.size.height * (2 / 3.0),
+        margin: EdgeInsets.only(left: 3, top: 5, right: 3, bottom: 5),
+        child: TextFormField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search, color: Colors.grey),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
     );
   }
 }
